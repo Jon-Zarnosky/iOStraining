0e17246782c78b75a4b89a0bc49d2c20f658b59d
Jon-Zarnosky <jzarnosky@anthropologie.com>
2015-11-18 15:50:05 -0500

    project setup

---
+++ iOStraining/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOStraining
+//
+//  Created by Jonathan Zarnosky on 11/18/15.
+//  Copyright © 2015 Jonathan Zarnosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOStraining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOStraining
+//
+//  Created by Jonathan Zarnosky on 11/18/15.
+//  Copyright © 2015 Jonathan Zarnosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
