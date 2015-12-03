c13f59b098088af3eb929e84aa8320011d6e722b
Jon-Zarnosky <jzarnosky@anthropologie.com>
2015-12-03 09:46:45 -0500

    12/3

--- iOStraining/ViewController.m
+++ iOStraining/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Car.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    Vehicle *v1 = [[Vehicle alloc] init];
+    // instance, allocate, initialize.
+    v1.make = @"Nissan";
+    v1.model = @"GT-R";
+    v1.name = @"Godzilla";
+    
+    Car *c1 = [[Car alloc] init];
+    c1.make = @"Pontiac";
+    c1.model = @"Trans Am";
+    c1.numberOfDoors = 2;
+    
+    Car *c2 = [[Car alloc] init];
+    [c2 setupVehicleValues];
+    c2.make = @"Ford";
+    c2.model = @"Flex";
+    c2.numberOfDoors = 4;
+    
+    Vehicle *v2 = [Vehicle new];
+    // need to understand designated initializer. Example: withFrame for table views
+    
+    NSLog(@"c1 values = %@, - %@/%@", c1, c1.make, c1.model);
+    NSLog(@"c2 values = %@, - %@/%@", c2, c2.make, c2.model);
 }
 
 - (void)didReceiveMemoryWarning {
